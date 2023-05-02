214b0a0c3132d23ab0e14c7a97e4e823795f19d5
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-05-02 17:53:40 +0600

    data make succefully

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const app = express()
 
 app.use(cors())
 const port =process.env.PORT || 5000
-const cuisines = require('./data/cuisines.json')
+const chefs = require('./data/chefs.json')
 
 
 app.get('/',(req,res)=>{
     res.send('Chef corners is running')
 })
-app.get('/cuisines',(req,res)=>{
-    res.send(cuisines)
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
+})
+app.get('/chefs/:id',(req,res)=>{
+    const id= req.params.id;
+    const chefDetails = chefs.find(c=>c.id===id);
+    res.send(chefDetails)
 })
 
 
